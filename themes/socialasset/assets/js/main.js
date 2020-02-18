0346415189c4065db88cdbf5bb2a6f0ba7ec93e1
shantoztech <45969365+shantoztech@users.noreply.github.com>
2020-02-19 04:10:57 +0600

    responsive

--- themes/socialasset/assets/js/main.js
+++ themes/socialasset/assets/js/main.js
@@ -227,7 +227,33 @@ if( $('.hm-partner-slider').length ){
       infinite: false,
       speed: 300,
       slidesToShow: 6,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
   });
 }
 
