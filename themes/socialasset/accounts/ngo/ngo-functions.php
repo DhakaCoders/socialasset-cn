8c31d34c9302e4b18363195a2a01af2e9e777be7
azizulsoftsw <azizul.softsw@gmail.com>
2020-02-24 15:53:33 +0600

    campaign create

--- themes/socialasset/accounts/ngo/ngo-functions.php
+++ themes/socialasset/accounts/ngo/ngo-functions.php
@@ -1,1 +1,78 @@
-<?php 
+<?php
+add_action('init', 'ngo_campaign_action_hook');
+
+function ngo_campaign_action_hook(){
+	if(is_user_logged_in()){
+		ngo_campaign_create();
+	}
+	
+}
+function ngo_campaign_create(){
+	global $msg;
+	if (isset( $_POST["add_campaign"] ) && wp_verify_nonce($_POST['ngo_add_campaign_nonce'], 'ngo-add-campaign-nonce')) {
+		$user = wp_get_current_user();
+		if(isset($_POST['campaign']) && $_POST['campaign'] == '-1'){
+			$campaigncat = '';
+		}else{
+			$campaigncat = $_POST['campaign'];
+		}
+		if(empty($msg)){
+			$post_information = array(
+				'post_author' => $user->ID,
+			    'post_title' => wp_strip_all_tags( $_POST['post_title'] ),
+			    'post_content' => $_POST['capmaign_content'],
+			    'post_type' => 'campaigns',
+			    'post_status' => 'publish'
+			);
+			 
+			$pid = wp_insert_post($post_information);
+
+			if(!empty($campaigncat)){
+				global $wpdb;
+				$object_id = (int) $pid;
+				$cat_id = (int) $campaigncat;
+				$wpdb->insert(
+		            $wpdb->term_relationships,
+		            array(
+		                'object_id'        => $object_id,
+		                'term_taxonomy_id' => $cat_id,
+		            )
+		        );
+			}
+			/*$campaign_tag = (isset($_POST['campaign_tag']) && !empty($_POST['campaign_tag']))? trim($_POST['campaign_tag']): '';
+			if(!empty($campaign_tag)){
+				global $wpdb;
+				$object_id = (int) $pid;
+				$spacial_offer_id = (int) $campaign_tag;
+				$wpdb->insert(
+		            $wpdb->term_relationships,
+		            array(
+		                'object_id'        => $object_id,
+		                'term_taxonomy_id' => $spacial_offer_id,
+		            )
+		        );
+			}*/
+			/*if(isset($_POST['attachment_id_array']) && !empty($_POST['attachment_id_array'])){
+				$thumbnail_ids = implode(',', $_POST['attachment_id_array']);
+				if ( ! add_post_meta( $pid, '_thumbnail_ids', $thumbnail_ids, true ) ) { 
+				   update_post_meta ( $pid, 'thumbnail_ids', $thumbnail_ids );
+				}
+			}
+			if(isset($_POST['_thumbnail_id'])){
+				set_post_thumbnail( $pid, $_POST['_thumbnail_id'] );
+			}*/
+			if(!empty($_POST['fromt_date']) && !empty($_POST['to_date'])){
+				add_post_meta( $pid, 'capmpaign_from_date', $_POST['fromt_date'], true );
+				add_post_meta( $pid, 'capmpaign_to_date', $_POST['to_date'], true );
+			}
+			add_post_meta( $pid, '_capmpaign_status', 'draft', true );
+			add_post_meta( $pid, '_supported_count', '0', true );
+			add_post_meta( $pid, '_supporter_ids', '', true );
+			$msg['success'] = 'Campaign saved successfully';
+		}else{
+			$msg['error'] = 'Could not save';
+		}
+		return $msg;
+	}
+	return false;
+}
---
+++ themes/socialasset/accounts/ngo/ngo-profile.php
@@ -0,0 +1,180 @@
+<div id="tab-1" class="">
+  <div class="tab-con-inr xs-center-width">
+    <?php 
+      if( isset($umetas['_user_account_status']) && !empty($umetas['_user_account_status']) ){
+        if($umetas['_user_account_status'] == 'draft'){
+    ?>
+    <div class="profile-is-draft">
+      <p><strong>Your profile is DRAFT</strong>   Lorem ipsum donor sit met.</p>
+      <i class="fas fa-times"></i>
+    </div>
+    <?php } }?>
+
+    <?php 
+      if(isset($msg) && array_key_exists("error",$msg)){ 
+        printf('<div class="profile-is-draft"><p><strong>%s</strong></p><i class="fas fa-times"></i></div>', $msg['error']);
+      }
+      if(isset($msg) && array_key_exists("success",$msg)){ 
+        printf('<div class="profile-is-draft"><p><strong>%s</strong></p><i class="fas fa-times"></i></div>', $msg['success']);
+      }
+    ?>
+    <div class="tab-con-title">
+      <strong>Account Details</strong>
+    </div>
+    
+      <div class="clearfix tab-con-col-row">
+        <form action="" method="post">
+        <div class="tab-con-col-4">
+          <div class="has-bx-shadow profile-basic-info-bx-cntlr text-center">
+            <div class="profile-edit-step-1 profile-img-step-1">
+              <div class="profile-img branding-logo" id="profile-priview">
+                <?php 
+                if( isset($umetas['_profile_logo_id']) && !empty($umetas['_profile_logo_id']) ){
+                  echo get_user_profile_logo_tag($umetas['_profile_logo_id']); 
+                }
+                ?>
+              </div>
+              <input type="hidden" id="_profile_logo_id" name="_profile_logo_id" value="">
+            </div>
+            <div class="profile-edit-step-2 profile-img-step-2">
+              <div class="profile-img-edit">
+                <input type="button" name="" value="" id="choose-file">
+                <label for="choose-file">
+                  <i><img src="<?php echo THEME_URI; ?>/assets/images/plus-icon-2.png"></i>
+                  <span class="file-up-instruction-txt">CLICK TO ADD<br> YOUR LOGO</span>
+                </label>
+              </div>
+            </div>
+
+            <?php 
+            $ngoname = '';
+            if( isset($umetas['_ngo_name']) && !empty($umetas['_ngo_name']) ){
+              printf('<strong>%s</strong>', $umetas['_ngo_name']);
+              $ngoname = $umetas['_ngo_name'];
+            }
+            ?>
+            <?php if(isset($user->user_email) && !empty($user->user_email)): ?>
+            <span style="display: block;"><?php echo $user->user_email; ?></span>
+            <?php endif;?>
+            <?php 
+            if( isset($umetas['_user_account_status']) && !empty($umetas['_user_account_status']) ){
+              if($umetas['_user_account_status'] == 'draft'){
+                printf('<span class="status-published-title color-red">STATUS: %s</span>', $umetas['_user_account_status']);
+              }else{
+                printf('<span class="status-published-title">STATUS: %s</span>', $umetas['_user_account_status']);
+              }
+            }
+            ?>
+
+            <div class="plr-30 profile-edit-step-2">
+                <div class="fl-input-field-row clearfix sa-input text-left username-filed">
+                <label>Username</label>
+                <input id="get_username" type="text" name="_ngo_name" value="<?php echo $ngoname; ?>">
+              </div>
+              <div class="fl-input-field-row clearfix sa-input text-left username-filed">
+                <label>About Your NGO</label>
+                <textarea name="_about_ngo" placeholder="Type a brief about your NGO"><?php if( isset($umetas['_about_ngo']) && !empty($umetas['_about_ngo']) ){
+                    printf('%s', $umetas['_about_ngo']);
+                  }?></textarea>
+              </div>
+            </div>
+            <div style="height: 1px"></div>
+            <hr class="clearfix">
+            <div class="plr-30">
+              <div class="profile-submit-btn profile-edit-step-1 flx-btn-center">
+                <!-- <input type="submit" name="" value="Edit Profile"> -->
+                <a class="edit-profile-btn" href="javascript:void(0)">Edit Profile</a>
+              </div>
+              <input type="hidden" name="user_change_profile_image_nonce" value="<?php echo wp_create_nonce('user-change-profile-image-nonce'); ?>"/>
+              <div class="profile-submit-btn profile-edit-step-2 flx-btn-center clearfix">
+                
+                <input type="submit" name="save_profile_logo" value="Save Changes">
+                <!-- <input id="edit-profile-cancle-btn" type="reset" name="" value="Cancel"> -->
+                <a href="javascript:void(0)" id="edit-profile-cancle-btn" href="#">Cancel</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        </form>
+        <div class="tab-con-col-8">
+          <div class="has-bx-shadow profile-rgt-info-bx-cntlr">
+            <strong class="profile-rgt-info-bx-title prib-plr">Change Password</strong>
+            <form action="" method="post" id="change_pass_form">
+            <div class="prib-plr profile-rgt-info-bx-input-fields">
+              <div class="sa-input clearfix pro-rgt-info-bx-input-fields-row">
+                <label>Current Password</label>
+                <input type="password" name="currnt_pass" required="required">
+              </div>
+            </div>
+            <div class="prib-plr profile-rgt-info-bx-input-fields">
+              <div class="sa-input clearfix pro-rgt-info-bx-input-fields-row">
+                <label>New Password</label>
+                <input type="password" name="new_pass" id="newpass" required="required"> 
+              </div>
+            </div>
+            <div class="prib-plr profile-rgt-info-bx-input-fields">
+              <div class="sa-input clearfix pro-rgt-info-bx-input-fields-row">
+                <label>Confirm New Password</label>
+                <input type="password" name="confpass" id="confpass" required="required">
+              </div>
+              <div class="pass-error-new">
+              <span class="error" style="color:red;display: block;"></span>
+            </div>
+            </div>
+            <div class="fl-forget-row prib-plr">
+              <a href="#">Forgot your password?</a>
+            </div>
+            <div class="prib-plr">
+              <div class="prib-btns profile-submit-btn clearfix">
+                <input type="hidden" name="user_change_password_nonce" value="<?php echo wp_create_nonce('user-change-password-nonce'); ?>"/>
+                <input type="submit" name="change_pass" value="Change Password">
+                <input type="reset" name="" value="Cancel">
+              </div>
+            </div>
+            </form>
+            <hr>
+            <strong class="profile-rgt-info-bx-title prib-plr">Notification Settings</strong>
+            <div class="switch-checkbox-con prib-plr">
+              <form>
+              <div class="switch-item">
+                <div class="switch-checkbox">
+                  <input type="checkbox" checked id="checkbox-switch1">
+                  <span class="checkbox-slider"></span>
+                </div>
+                <label for="checkbox-switch1">I would like to receive notification through Email</label>
+              </div>
+              <div class="switch-item">
+                <div class="switch-checkbox">
+                  <input type="checkbox" id="checkbox-switch2">
+                  <span class="checkbox-slider"></span>
+                </div>
+                <label for="checkbox-switch2">Newsletters</label>
+              </div>
+              <div class="switch-item">
+                <div class="switch-checkbox">
+                  <input type="checkbox" id="checkbox-switch3" value="">
+                  <span class="checkbox-slider"></span>
+                </div>
+                <label for="checkbox-switch3">Emails for new campaigns</label>
+              </div>
+              <div class="switch-item">
+                <div class="switch-checkbox">
+                  <input type="checkbox" id="checkbox-switch4">
+                  <span class="checkbox-slider"></span>
+                </div>
+                <label for="checkbox-switch4">Lorem ipsum</label>
+              </div>
+            </div>
+            <div style="height: 15px;"></div>
+            <hr>
+            <div class="prib-plr">
+              <div class="profile-submit-btn submit-btn-lrg profile-submit-btn-center clearfix">
+                <input type="submit" name="" value="SUBMIT">
+              </div>
+            </div>
+            </form>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
